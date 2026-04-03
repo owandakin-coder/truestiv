4bfe91dd6b0927b6717933fb4f07026e442f4660
owandakin-coder <owandakin@gmail.com>
2026-04-03 20:27:19 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    BASE_URL: str = os.getenv("BASE_URL", "http://localhost:8000")
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
